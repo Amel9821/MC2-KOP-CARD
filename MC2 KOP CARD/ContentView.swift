4ad3dfc94faab84729b5e2e01bfa7d7b83f638ee
Amel9821 <amalia21198@gmail.com>
2022-06-26 10:35:18 +0700

    Initial Commit

---
+++ MC2 KOP CARD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC2 KOP CARD
+//
+//  Created by Amalia . on 26/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC2 KOP CARD/MC2_KOP_CARDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_KOP_CARDApp.swift
+//  MC2 KOP CARD
+//
+//  Created by Amalia . on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MC2_KOP_CARDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
