52f52b20c1446f0355aa87e1e1439eee9e75a13c
Amel9821 <amalia21198@gmail.com>
2022-06-27 08:44:04 +0700

    Making login and register view

---
+++ MC2 KOP CARD/View/LoginView.swift
@@ -0,0 +1,57 @@
+//
+//  LoginView.swift
+//  MC2 KOP CARD
+//
+//  Created by Amalia . on 26/06/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image("GambarKartu")
+                    .resizable()
+                    .frame(width: 104, height: 141, alignment: .center)
+                Text("KOP CARD")
+                    .font(.custom("Krungthep", size: 30))
+                    .foregroundColor(Color("Color"))
+                    
+                TextField("Email", text: $email)
+                    .padding()
+                    .frame(width: 280, height: 50, alignment: .center)
+                    .background(.gray.opacity(0.1))
+                    .padding()
+                TextField("Password", text: $password)
+                    .padding()
+                    .frame(width: 280, height: 50, alignment: .center)
+                    .background(.gray.opacity(0.1))
+                    .padding()
+                Text("Sign in").font(Font.system(size: 20, design: .rounded))
+                    .padding()
+                    .frame(width: 280, height: 50, alignment: .center)
+                    .background(Color("Color"))
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .padding()
+                HStack {
+                    Text("Don't have an account?")
+                    NavigationLink(destination: RegisterView(), label: {
+                        Text("Register now!")
+                            .foregroundColor(Color("Color"))
+                    }
+                    )
+                }
+            }
+        }
+    }
+    struct LoginView_Previews: PreviewProvider {
+        static var previews: some View {
+            LoginView()
+        }
+    }
+}
---
+++ MC2 KOP CARD/View/RegisterView.swift
@@ -0,0 +1,61 @@
+//
+//  RegisterView.swift
+//  MC2 KOP CARD
+//
+//  Created by Amalia . on 26/06/22.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State var name: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var username: String = ""
+    
+    var body: some View {
+        VStack {
+            Image("GambarKartu")
+                .resizable()
+                .frame(width: 104, height: 141, alignment: .center)
+            Text("KOP CARD")
+                .font(.custom("Krungthep", size: 30))
+                .foregroundColor(Color("Color"))
+                
+            TextField("Name", text: $name)
+                .padding()
+                .frame(width: 280, height: 50, alignment: .center)
+                .background(.gray.opacity(0.1))
+                .padding()
+            TextField("Email", text: $email)
+                .padding()
+                .frame(width: 280, height: 50, alignment: .center)
+                .background(.gray.opacity(0.1))
+                .padding()
+            TextField("Password", text: $password)
+                .padding()
+                .frame(width: 280, height: 50, alignment: .center)
+                .background(.gray.opacity(0.1))
+                .padding()
+            TextField("Username", text: $username)
+                .padding()
+                .frame(width: 280, height: 50, alignment: .center)
+                .background(.gray.opacity(0.1))
+                .padding()
+            Text("Register").font(Font.system(size: 20, design: .rounded))
+                .padding()
+                .frame(width: 280, height: 50, alignment: .center)
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .foregroundColor(.white)
+                .padding()
+        }
+        
+    }
+    
+    struct RegisterView_Previews: PreviewProvider {
+        static var previews: some View {
+            RegisterView()
+        }
+    }
+}
