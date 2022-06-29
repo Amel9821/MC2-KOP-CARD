200659ef35a9702ffba17e344a82a41947ec5703
Amel9821 <amalia21198@gmail.com>
2022-06-29 18:22:46 +0700

    Bikin Scanning QR

--- MC2 KOP CARD/ContentView.swift
+++ MC2 KOP CARD/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    @State private var isPresentingScanner = false
+    @State private var scannedCode: String = "Scan a QR code to get started."
+    
+    var scannerSheet : some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
     }
-}
+    
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            Button("Scan QR Code") {
+                self.isPresentingScanner = true
+        }
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannerSheet
+            }
+        }
+    }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
---
+++ MC2 KOP CARD/QRGenerateView.swift
@@ -0,0 +1,23 @@
+//
+//  QRGenerateView.swift
+//  MC2 KOP CARD
+//
+//  Created by Amalia . on 28/06/22.
+//
+
+import SwiftUI
+
+struct QRGenerateView: View {
+    
+    @State private var text = ""
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QRGenerateView_Previews: PreviewProvider {
+    static var previews: some View {
+        QRGenerateView()
+    }
+}
