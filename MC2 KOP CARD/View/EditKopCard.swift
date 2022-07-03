f947322e9c097506e2b2da109ecd9c246edafa0e
Amel9821 <amalia21198@gmail.com>
2022-07-03 11:39:41 +0700

    Nambain editkopcard

--- MC2 KOP CARD/View/EditKopCard.swift
+++ MC2 KOP CARD/View/EditKopCard.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct EditKopCard: View {
     
     @State private var Name : String = "Pieter"
     @State private var Username : String = "PieterNCT"
@@ -20,66 +20,66 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-                VStack{
-                    Image("addProfile")
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                    List{
-                        HStack{
-                            Text("Name")
-                            TextField("Jasmine Kpoper", text: $Name)
-                                .foregroundColor(Color("DPurple"))
-                                .padding(.leading, 21)
-                        }
-                        HStack{
-                            Text("Username")
-                            TextField("Jasmine Kpoper", text: $Username)
-                                .multilineTextAlignment(.trailing)
-                                .accentColor(.yellow)
-                                .foregroundColor(Color("DPurple"))
-                        }
-                        HStack {
-                            Text("Bio")
-                            TextField("Jasmine Kpoper", text: $Bio)
-                                .lineLimit(4)
-                                .multilineTextAlignment(.trailing)
-                                .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity, alignment: .trailing)
-                                .foregroundColor(Color("DPurple"))
-                        }
-                        HStack{
-                            Text("Twitter")
-                            TextField("Jasmine Kpoper", text: $Twitter)
-                                .multilineTextAlignment(.trailing)
-                                .foregroundColor(Color("DPurple"))
-                            
-                        }
-                        HStack{
-                            Text("Instagram")
-                            TextField("Jasmine Kpoper", text: $Instagram)
-                                .multilineTextAlignment(.trailing)
-                                .foregroundColor(Color("DPurple"))
-                            
-                        }
-                        HStack{
-                            Text("Line")
-                            TextField("Jasmine Kpoper", text: $Line)
-                                .multilineTextAlignment(.trailing)
-                                .foregroundColor(Color("DPurple"))
-                        }
-                        HStack{
-                            Text("KakaoTalk")
-                            TextField("Jasmine Kpoper", text: $KakaoTalk)
-                                .multilineTextAlignment(.trailing)
-                                .foregroundColor(Color("DPurple"))
-                        }
-                        HStack{
-                            Text("Telegram")
-                            TextField("Jasmine Kpoper", text: $Telegram)
-                                .multilineTextAlignment(.trailing)
-                                .foregroundColor(Color("DPurple"))
-                        }
+            VStack{
+                Image("addProfile")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                List{
+                    HStack{
+                        Text("Name")
+                        TextField("Jasmine Kpoper", text: $Name)
+                            .foregroundColor(Color("DPurple"))
+                            .padding(.leading, 21)
                     }
-                    .listStyle(.plain)
+                    HStack{
+                        Text("Username")
+                        TextField("Jasmine Kpoper", text: $Username)
+                            .multilineTextAlignment(.trailing)
+                            .accentColor(.yellow)
+                            .foregroundColor(Color("DPurple"))
+                    }
+                    HStack {
+                        Text("Bio")
+                        TextField("Jasmine Kpoper", text: $Bio)
+                            .lineLimit(4)
+                            .multilineTextAlignment(.trailing)
+                            .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity, alignment: .trailing)
+                            .foregroundColor(Color("DPurple"))
+                    }
+                    HStack{
+                        Text("Twitter")
+                        TextField("Jasmine Kpoper", text: $Twitter)
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(Color("DPurple"))
+                        
+                    }
+                    HStack{
+                        Text("Instagram")
+                        TextField("Jasmine Kpoper", text: $Instagram)
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(Color("DPurple"))
+                        
+                    }
+                    HStack{
+                        Text("Line")
+                        TextField("Jasmine Kpoper", text: $Line)
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(Color("DPurple"))
+                    }
+                    HStack{
+                        Text("KakaoTalk")
+                        TextField("Jasmine Kpoper", text: $KakaoTalk)
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(Color("DPurple"))
+                    }
+                    HStack{
+                        Text("Telegram")
+                        TextField("Jasmine Kpoper", text: $Telegram)
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(Color("DPurple"))
+                    }
+                }
+                .listStyle(.plain)
             }
             .navigationTitle("Edit profile")
             .navigationBarTitleDisplayMode(.inline)
@@ -87,10 +87,10 @@ struct ContentView: View {
         }
     }
 }
-struct ContentView_Previews: PreviewProvider {
+struct EditKopCard_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
+            EditKopCard()
                 .previewInterfaceOrientation(.portrait)
             //            ContentView()
         }
