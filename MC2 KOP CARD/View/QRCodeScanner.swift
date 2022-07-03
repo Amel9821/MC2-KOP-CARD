18f91fb884215486c3f10da2faa6c59cd67053a1
Amel9821 <amalia21198@gmail.com>
2022-07-03 11:50:08 +0700

    Nambain QRcode scanner and package

---
+++ MC2 KOP CARD/View/QRCodeScanner.swift
@@ -0,0 +1,43 @@
+//
+//  QRCodeScanner.swift
+//  MC2 KOP CARD
+//
+//  Created by Amalia . on 03/07/22.
+//
+
+import SwiftUI
+import CodeScanner
+
+struct QRCodeScanner: View {
+    @State private var isPresentingScanner = false
+    @State private var scannedCode: String = "Scan a QR code to get started."
+    
+    var scannerSheet : some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
+    }
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            Button("Scan QR Code") {
+                self.isPresentingScanner = true
+        }
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannerSheet
+            }
+        }
+    }
+struct QRCodeScanner_Previews: PreviewProvider {
+    static var previews: some View {
+        QRCodeScanner()
+    }
+}
+}
+
